f07fb793be750520625c60bf8d780757df3c16b3
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-26 10:24:53 -0400

    Refactor code structure and add location permission handling

--- core/actions/permissions/location.ts
+++ core/actions/permissions/location.ts
@@ -1,3 +1,4 @@
+import { Alert, Linking } from 'react-native';
 import * as Location from 'expo-location';
 import { PermissionStatus } from '@/infrastructure/interfaces/location';
 
@@ -6,7 +7,10 @@ export const requestLocationPermission =
     const { status } = await Location.requestForegroundPermissionsAsync();
 
     if (status !== 'granted') {
-      manualPermissionRequest();
+      if (status === 'denied') {
+        manualPermissionRequest();
+      }
+
       return PermissionStatus.DENIED;
     }
 
@@ -27,5 +31,20 @@ export const checkLocationPermission = async () => {
 };
 
 const manualPermissionRequest = async () => {
-  //TODO: Lanzar los ajustes de la aplicación
+  Alert.alert(
+    'Permiso de ubicación necesario',
+    'Para continuar debe de habilitar el permiso de location en los ajustes de la app',
+    [
+      {
+        text: 'Abrir ajustes',
+        onPress: () => {
+          Linking.openSettings();
+        },
+      },
+      {
+        text: 'Cancel',
+        style: 'destructive',
+      },
+    ]
+  );
 };
