986669ab6f7641e267a32f455b2610231bacd28c
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-26 12:20:13 -0400

    Refactor code structure and add Zustand for state management

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 const MapScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Map Screen</Text>
+    <View style={styles.container}>
+      <MapView
+        // showsPointsOfInterest={false}
+        style={styles.map}
+        // 45.412560, -75.698931
+        // provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 45.41256,
+          longitude: -75.698931,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+    // backgroundColor: 'red',
+  },
+});
